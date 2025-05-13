f8eb628e022222bb679e11014c7244d916c885f9
ya2min3 <yasminebelbahri@gmail.com>
2025-05-13 23:13:28 +0200

    fixed comment

--- qwiz.js
+++ qwiz.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Gestion de l'affichage/masquage du mot de passe pour la page d'inscription (signup.php)
     const togglePasswordSignup = document.querySelector("#togglePassword-signup");
-    const passwordInputSignup = document.querySelector("#password"); // Notez que l'ID est le même sur les deux pages
+    const passwordInputSignup = document.querySelector("#password"); // l'ID est le même sur les deux pages
 
     if (togglePasswordSignup && passwordInputSignup) {
         togglePasswordSignup.addEventListener("click", function () {
